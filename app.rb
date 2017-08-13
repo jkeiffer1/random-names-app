5efded6d5d02c4e81ca186ef84ace0c317133ba2
jkeiffer1 <saros82@hotmail.com>
2017-08-13 12:12:22 -0400

    making sure i pushed, added style file

--- app.rb
+++ app.rb
@@ -1,2 +1,80 @@
 require "sinatra"
 require_relative "random_pair.rb"
+
+get '/' do
+	message = params[:message]
+	
+	erb :login, locals:{message: message}
+end
+
+post '/login' do
+	correct_login = {'jkeiffer' => "1234", 'scstew' => "4321", 'ldeems' => "1233"}
+	username = params[:username]
+	password = params[:password]
+	message = "Incorrect Username or Password, please try again."
+	success = "Login Successful"
+	#{}"#{correct_login} #{newusername} newuser,new pass is #{newpassword}"
+	#p "#{correct_login} correctlog"
+	#p "#{loginhash} loghash"
+	#p loginhash.class
+	 correct_login.each_pair do |key, value|
+	# 	p "#{key}#{value}"
+		if username == key && password == value
+			redirect '/names?success=' + success
+		elsif username == key
+			message = "Incorrect password!"
+			redirect '/?message=' + message
+		elsif password == value
+			message = "Incorrect username!"
+			redirect '/?message=' + message
+		end
+		message = "Incorrect username and password!"
+		redirect '/?message=' + message
+	
+	 end
+end
+
+
+
+get '/names' do
+	success = params[:success]
+	erb :names, locals:{success: success}
+end
+
+post '/names' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	success = params[:success]
+	redirect '/randnames?firstname=' + firstname + "&lastname=" + lastname
+end
+
+get '/randnames' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	erb :randnames, locals:{firstname: firstname, lastname: lastname}
+end
+
+post '/randnames' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	rando = params.values
+	p "rando here#{rando}"
+	randfunc = random_pair(rando)
+	p "randfunc is here#{randfunc}"
+	strng = stringer(randfunc)
+	p "string is here #{strng}"
+	redirect '/results?firstname=' + firstname + "&lastname=" + lastname + "&strng=" + strng
+end
+
+get '/results' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	strng = params[:strng]
+	erb :results, locals:{firstname: firstname, lastname: lastname}
+end
+
+post '/results' do
+	firstname = params[:firstname]
+	lastname = params[:lastname]
+	redirect '/randnames?firstname=' + firstname + "&lastname=" + lastname
+end
--- random_pair.rb
+++ random_pair.rb
@@ -1,54 +1,66 @@
- # def random_pair(names)
- # 	array = names.shuffle.each_slice(2).to_a
- # 	x = array.length - 1
- # 	if names.length % 2 == 0
- # 		array.length
- # 	else
- # 		array[0] << array[x]
- # 		array.pop
- # 		array[0].flatten!
-
- # 	end
- # 	p "#{array}"
- # 	p array.length
-
- # end
-
-
- def random_pair_no_slice(name_arr)
- 	if name_arr.length <= 3
- 		p "Insufficient Names"
+ def random_pair(names)
+ 	array = names.shuffle.each_slice(2).to_a
+ 	x = array.length - 1
+ 	if names.length % 2 == 0
+ 		array.length
  	else
- 		name_arr.shuffle!
- 		arr_1 = []
- 		arr_2 = []
+ 		array[0] << array[x]
+ 		array.pop
+ 		array[0].flatten!
 
-	 	counter = 1
+ 	end
+ 	p "#{array}"
+ 	p array
 
-	 	name_arr.each do |x|
-	 		if counter % 2 == 0
-	 			arr_1 << x
-	 		else
-	 			arr_2 << x
-	 		end
+ end
 
-	 	counter += 1
+ def stringer(string)
+ 	heresstring = ""
+ 	string.each do |lilarray|
+ 		if lilarray.length == 3
+ 			x = "#{lilarray[0]} and #{lilarray[1]} and #{lilarray[2]}"
+ 		else
+ 			x = "#{lilarray[0]} and #{lilarray[1]}"
+ 		end
+ 		heresstring << x
+ 	end
+ 	heresstring
+ end
+
+#  def random_pair_no_slice(name_arr)
+#  	if name_arr.length <= 3
+#  		p "Insufficient Names"
+#  	else
+#  		name_arr.shuffle!
+#  		arr_1 = []
+#  		arr_2 = []
+
+# 	 	counter = 1
+
+# 	 	name_arr.each do |x|
+# 	 		if counter % 2 == 0
+# 	 			arr_1 << x
+# 	 		else
+# 	 			arr_2 << x
+# 	 		end
+
+# 	 	counter += 1
 	 	
-	 	end
-	 	#p "#{arr_1} arr_1 is here"
-	 	#p "#{arr_2} arr_2 is here"
-	 	big_arr = arr_1.zip(arr_2)
-	 	#p "#{big_arr} big arr here"
+# 	 	end
+# 	 	#p "#{arr_1} arr_1 is here"
+# 	 	#p "#{arr_2} arr_2 is here"
+# 	 	big_arr = arr_1.zip(arr_2)
+# 	 	#p "#{big_arr} big arr here"
 
-	 	if name_arr.length % 2 == 0
-	 	else
-	 		big_arr.last << name_arr.last
-	 	end
+# 	 	if name_arr.length % 2 == 0
+# 	 	else
+# 	 		big_arr.last << name_arr.last
+# 	 	end
 
-	 	p "#{big_arr} here i am"
-	 	big_arr.length
-	 end
-end
+# 	 	p "#{big_arr} here i am"
+# 	 	big_arr.length
+# 	 end
+# end
 
  
 
